cff79a78f5a415b087bdabca9ac51e3a565bcc09
Tomasz Sułkowski <Tomasz Sukowski>
2017-04-27 00:08:27 +0200

    LFS window capture with ROI + vision algorithm WIP

--- AutoCruise/Cruiser.cs
+++ AutoCruise/Cruiser.cs
@@ -7,6 +7,10 @@ using System.Threading;
 using System.Windows;
 using AutoCruise.Main;
 using AutoCruise.ScreenCapture;
+using Emgu.CV.UI;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
 
 namespace AutoCruise
 {
@@ -14,14 +18,20 @@ namespace AutoCruise
     {
         private CancellationTokenSource _cancelToken;
         private Thread _cruiseThread;
+        private ImageViewer _imageViewer;
+
+        int width = 640;
+        int height = 480;
 
         public Cruiser()
         {
             _cancelToken = new CancellationTokenSource();
+            _imageViewer = new ImageViewer() { Width = width, Height = height };
         }
 
         public void StartCruising()
         {
+            _imageViewer.Show();
             DateTime time = DateTime.Now;
             int frameCounter = 0;
 
@@ -62,13 +72,59 @@ namespace AutoCruise
         {
             using (var screenShot = screenCapture.GetScreenShot())
             {
+                var img = new Image<Rgb, UInt16>(screenShot).Convert<Gray, Byte>();
+                img = ResizeCrop(img);
+                img = img.Copy(new Rectangle(0, 160, width, height - 160 - 120));
+                //img = img.PyrDown().PyrUp();
+                //img = Hough(img);
 
+                _imageViewer.Image = img;
             }
+        }
+
+        private Image<Gray, byte> Hough(Image<Gray, byte> img)
+        {
+            return img.Canny(50, 120);
+            //var houghLines = img.HoughLines(40, 60, 4, Math.PI / 180, 3, 30, 3);
+            //foreach(var houghLine in houghLines)
+            //{
+            //    foreach(var segment in houghLine)
+            //    {
+            //        img.Draw(segment, new Gray(255), 3);
+            //    }
+            //}
+            return img;
+        }
+
+        private Image<Gray, Byte> ResizeCrop(Image<Gray, Byte> img)
+        {
+            int ratio = img.Width * 100 / img.Height;
+            int newWidth = width;
+            int newHeight = height;
+
+            if (ratio > (width * 100 / height))
+            {
+                newWidth = height * ratio / 100;
+            }
+            else
+            {
+                newHeight = width * 100 / ratio;
+            }
+
+            img = img.Resize(newWidth, newHeight, Emgu.CV.CvEnum.Inter.Linear);
+
+            return img.Copy(new Rectangle((newWidth - width) / 2, (newHeight - height) / 2, width, height));
         }
 
         public void Dispose()
         {
             _cancelToken.Cancel();
+            Thread.Sleep(500);
+            if (_cruiseThread != null)
+            {
+                _cruiseThread.Abort();
+            }
+            _imageViewer.Dispose();
         }
     }
 }
--- AutoCruise/ScreenCapture/GraphicsScreenCapture.cs
+++ AutoCruise/ScreenCapture/GraphicsScreenCapture.cs
@@ -6,33 +6,62 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Windows;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace AutoCruise.ScreenCapture
 {
     public class GraphicsScreenCapture : IScreenCapture
     {
-        private int _width;
-        private int _height;
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
 
-        public GraphicsScreenCapture()
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
         {
-            _width = (int)SystemParameters.PrimaryScreenWidth;
-            _height = (int)SystemParameters.PrimaryScreenHeight;
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
         }
 
         public Bitmap GetScreenShot()
         {
-            Bitmap bmpScreenCapture = new Bitmap(_width, _height);
+            var bounds = GetBounds();
+
+            Bitmap bmpScreenCapture = new Bitmap((int)bounds.Width, (int)bounds.Height);
 
             using (Graphics g = Graphics.FromImage(bmpScreenCapture))
             {
-                g.CopyFromScreen(_width, _height,
-                                 0, 0,
+                g.CopyFromScreen((int)bounds.Left, (int)bounds.Top, 0, 0,
                                  bmpScreenCapture.Size,
                                  CopyPixelOperation.SourceCopy);
             }
 
             return bmpScreenCapture;
+        }
+
+        IntPtr _gamehWnd = IntPtr.Zero;
+        private Rectangle GetBounds()
+        {
+            if (_gamehWnd == IntPtr.Zero)
+            {
+                _gamehWnd = FindWindow(null, "Live for Speed");
+            }
+
+            if (_gamehWnd != IntPtr.Zero)
+            {
+                RECT rct = new RECT();
+                if (GetWindowRect(_gamehWnd, out rct))
+                {
+                    return new Rectangle((int)rct.Left+10, (int)rct.Top+30, (int)rct.Right - (int)rct.Left -20, (int)rct.Bottom - (int)rct.Top -30);
+                }
+            }
+
+            return new Rectangle(0, 0, (int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight);
         }
 
         public void Dispose()
