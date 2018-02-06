6ae9ebdd1a4fccbc28c0b33fcca9f158102170d8
soolek <soolek@gmail.com>
2018-02-06 23:37:39 +0100

    Not working SharpDX ffb

--- AutoCruise/Control/FfbWheelIntermediaryControl.cs
+++ AutoCruise/Control/FfbWheelIntermediaryControl.cs
@@ -1,33 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using SharpDX.DirectInput;
+using System;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using vJoyInterfaceWrap;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace AutoCruise.Control
 {
     public class FfbWheelIntermediaryControl : IControl
     {
         public IControl OutputControl { get; private set; }
+        public Joystick Wheel { get; private set; }
+
+        private Guid _wheelGuid;
+        private Effect _wheelEffect;
 
         public FfbWheelIntermediaryControl(IControl outputControl)
         {
             OutputControl = outputControl;
+
+            var directInput = new DirectInput();
+            var device = directInput.GetDevices(DeviceType.Driving, DeviceEnumerationFlags.ForceFeedback).FirstOrDefault();
+            _wheelGuid = device.InstanceGuid;
+            Wheel = new Joystick(directInput, _wheelGuid);
+
+            MessageBox.Show(string.Format("Wheel '{0}' found with ffb: {1}", device.ProductName, Wheel.GetEffects().Select(e => e.Name).Aggregate((a, b) => a + "," + b)));
+            //wheel.Properties.BufferSize=128;
+        }
+
+        private bool _initialized = false;
+        public void Initialize()
+        {
+            if(_initialized)
+            {
+                return;
+            }
+            Wheel.SetCooperativeLevel(new WindowInteropHelper(Application.Current.MainWindow).Handle, CooperativeLevel.Exclusive | CooperativeLevel.Background);
+            Wheel.Acquire();
+            _initialized = true;
+        }
+
+        public void SetLateral(float lateral)
+        {
+            Initialize();
+            if (lateral == 0 && _wheelEffect != null)
+            {
+                _wheelEffect.Stop();
+            }
+            else
+            {
+                var effectParameters = new EffectParameters()
+                {
+                    Directions = new[] { 0 },
+                    Axes = new[] { 0 },
+                    Flags = EffectFlags.Cartesian | EffectFlags.ObjectOffsets,
+                    Duration = int.MaxValue,
+                    SamplePeriod = 0,
+                    Gain = 10000,
+                    Envelope = null,
+                    Parameters = new ConstantForce()
+                    {
+                        Magnitude = -5000
+                    }
+                };
+                _wheelEffect = new Effect(Wheel, _wheelGuid, effectParameters);
+                _wheelEffect.Start(1, EffectPlayFlags.None);
+            }
+
+            OutputControl.SetLateral(lateral);
         }
 
         public void SetLongitudal(float longitudal)
         {
             OutputControl.SetLongitudal(longitudal);
-        }
-
-        public void SetLateral(float lateral)
-        {
-            OutputControl.SetLateral(lateral);
         }
 
         public void ShiftUp()
