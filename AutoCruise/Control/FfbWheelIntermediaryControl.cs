4a2252796cb8d18cc899d1b462efb7c05f333106
soolek <soolek@gmail.com>
2018-02-07 00:31:13 +0100

    Working Ffb using Logitech SDK

--- AutoCruise/Control/FfbWheelIntermediaryControl.cs
+++ AutoCruise/Control/FfbWheelIntermediaryControl.cs
@@ -1,6 +1,7 @@
 ﻿using SharpDX.DirectInput;
 using System;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Interop;
 
@@ -9,61 +10,32 @@ namespace AutoCruise.Control
     public class FfbWheelIntermediaryControl : IControl
     {
         public IControl OutputControl { get; private set; }
-        public Joystick Wheel { get; private set; }
-
-        private Guid _wheelGuid;
-        private Effect _wheelEffect;
 
         public FfbWheelIntermediaryControl(IControl outputControl)
         {
             OutputControl = outputControl;
-
-            var directInput = new DirectInput();
-            var device = directInput.GetDevices(DeviceType.Driving, DeviceEnumerationFlags.ForceFeedback).FirstOrDefault();
-            _wheelGuid = device.InstanceGuid;
-            Wheel = new Joystick(directInput, _wheelGuid);
-
-            MessageBox.Show(string.Format("Wheel '{0}' found with ffb: {1}", device.ProductName, Wheel.GetEffects().Select(e => e.Name).Aggregate((a, b) => a + "," + b)));
-            //wheel.Properties.BufferSize=128;
         }
 
-        private bool _initialized = false;
-        public void Initialize()
-        {
-            if(_initialized)
-            {
-                return;
-            }
-            Wheel.SetCooperativeLevel(new WindowInteropHelper(Application.Current.MainWindow).Handle, CooperativeLevel.Exclusive | CooperativeLevel.Background);
-            Wheel.Acquire();
-            _initialized = true;
-        }
-
+        private bool _logiInitialized = false;
         public void SetLateral(float lateral)
         {
-            Initialize();
-            if (lateral == 0 && _wheelEffect != null)
+            if (!_logiInitialized)
             {
-                _wheelEffect.Stop();
+                LogitechGSDK.LogiSteeringInitialize(true);
+                _logiInitialized = true;
             }
-            else
+
+
+            if (LogitechGSDK.LogiUpdate() && LogitechGSDK.LogiIsConnected(0))
             {
-                var effectParameters = new EffectParameters()
+                if (lateral == 0)
                 {
-                    Directions = new[] { 0 },
-                    Axes = new[] { 0 },
-                    Flags = EffectFlags.Cartesian | EffectFlags.ObjectOffsets,
-                    Duration = int.MaxValue,
-                    SamplePeriod = 0,
-                    Gain = 10000,
-                    Envelope = null,
-                    Parameters = new ConstantForce()
-                    {
-                        Magnitude = -5000
-                    }
-                };
-                _wheelEffect = new Effect(Wheel, _wheelGuid, effectParameters);
-                _wheelEffect.Start(1, EffectPlayFlags.None);
+                    LogitechGSDK.LogiStopSpringForce(0);
+                }
+                else
+                {
+                    LogitechGSDK.LogiPlaySpringForce(0, (int)(lateral * 90), 90, 90);
+                }
             }
 
             OutputControl.SetLateral(lateral);
@@ -97,6 +69,7 @@ namespace AutoCruise.Control
         public void Dispose()
         {
             OutputControl.Dispose();
+            LogitechGSDK.LogiSteeringShutdown();
         }
     }
 }
